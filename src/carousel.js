8fcaa96d2fc8d026da54896ee228a0144c0b3dc6
Torofms <torofms37@gmail.com>
2023-01-26 23:12:39 -0700

    1.5.3

---
+++ src/carousel.js
@@ -0,0 +1,27 @@
+var slideIndex = 0;
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Cambia de slide cada 2 segundos
+}
+
+// Mostrar la primera slide
+showSlides();
+
+// Botones de navegación
+var prevBtn = document.getElementById("prev-btn");
+var nextBtn = document.getElementById("next-btn");
+prevBtn.addEventListener("click", function() {
+  slideIndex = slideIndex - 2;
+  showSlides();
+});
+nextBtn.addEventListener("click", function() {
+  showSlides();
+});
